d7d8fdcc75e7cea13ae0af589655556d4fe4d3e8
Volodymyr Zenin <vovan1608@gmail.com>
2021-08-30 21:34:26 +0300

    added Modal component

--- book-store-app/src/components/AddAuthor.js
+++ book-store-app/src/components/AddAuthor.js
@@ -1,7 +1,14 @@
+import Modal from "./Modal";
+
 const AddAuthor = _ => {
+	const onClick = e => {
+		e.target.children[0].classList.remove('hide');
+	}
+
 	return (
-		<div className="text_path">
+		<div className="text_path" onClick={onClick} id="add_author">
 			Add Author
+			<Modal />
 		</div>
 	);
 }
