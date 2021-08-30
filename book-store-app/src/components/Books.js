4de4fa79be8db9d3422d3c162e8ba65ef4241bb3
Volodymyr Zenin <vovan1608@gmail.com>
2021-08-30 20:43:17 +0300

    addde search in Books component

--- book-store-app/src/components/Books.js
+++ book-store-app/src/components/Books.js
@@ -3,18 +3,34 @@ import { useState, useEffect } from 'react';
 import Search from "./Search";
 import Card from "./Card";
 
-const Books = _ => {
+const Books = () => {
 	const [books, setBooks] = useState([]);
+	const [searchExp, setSearchExp] = useState('');
+	const [filtered, setFiltered] = useState([]);
 
 	useEffect(_ => {
 		getAxios('/books').then(res => setBooks(res.data));
 	}, [setBooks]);
 
+	useEffect(() => {
+		const searchRegExp = new RegExp(`${searchExp}`, 'i');
+
+		if (searchExp) {
+			const filter = books
+				.filter(({author}) => searchRegExp.test(author));
+
+			setFiltered(filter);
+		}
+	}, [searchExp]);
+
+	const data = searchExp ? filtered : books;
+
+
 	return (
 		<main className="main">
-			<Search />
+			<Search passData={setSearchExp} />
 			<div className="books_container">
-				{books.map(book => {
+				{data.map(book => {
 					const {id, picture, name, author, description} = book;
 					return (
 						<Card
